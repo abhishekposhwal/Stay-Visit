974e2e142a0cf9270048004dda8887d0a68e02fa
Abhishek Poshwal <abhishek.poshwal987@gmail.com>
2025-09-08 14:16:50 +0000

    change color to gray color and text color to white (_for element <Button

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -159,7 +159,7 @@ export default function ProfilePage() {
                 <>
                   <div className="flex justify-between items-center mb-6">
                       <h1 className="text-2xl font-bold">About me</h1>
-                      <Button variant="outline">Edit</Button>
+                      <Button variant="secondary">Edit</Button>
                   </div>
                   
                   <div className="p-8 rounded-xl">
