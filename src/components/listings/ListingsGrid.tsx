6e91cf39de6bc136e635e1f7f03b8c9e9f7efa24
Abhishek Poshwal <abhishek.poshwal987@gmail.com>
2025-09-08 11:56:17 +0000

    make this cards similar size of home page cards (_for element div_)

--- src/components/listings/ListingsGrid.tsx
+++ src/components/listings/ListingsGrid.tsx
@@ -31,7 +31,7 @@ export default function ListingsGrid({ listings, layout = 'grid' }: ListingsGrid
   }
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
       {listings.map((property) => (
         <ListingCard key={property.id} property={property} />
       ))}
