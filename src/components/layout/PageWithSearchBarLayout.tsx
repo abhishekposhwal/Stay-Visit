e6b03bd1ad6d15d219cddafbaffb73cdf2b18777
Abhishek Poshwal <abhishek.poshwal987@gmail.com>
2025-09-08 09:20:24 +0000

    why scroll up and scroll down functionalities are not working in which s

--- src/components/layout/PageWithSearchBarLayout.tsx
+++ src/components/layout/PageWithSearchBarLayout.tsx
@@ -1,4 +1,6 @@
 
+'use client';
+
 import { SearchBar } from './SearchBar';
 
 export default function PageWithSearchBarLayout({
@@ -7,15 +9,15 @@ export default function PageWithSearchBarLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <>
+        <div>
             <div className="hidden md:block sticky top-16 z-30 pb-4 bg-background border-b">
-                <div className="-mt-16">
+                 <div className="-mt-16">
                     <SearchBar />
                 </div>
             </div>
             <div className="pt-8">
                 {children}
             </div>
-        </>
+        </div>
     );
 }
