4811237d4035c61fbbcda72cd8dd37d721d676ac
Abhishek Poshwal <abhishek.poshwal987@gmail.com>
2025-09-08 12:12:27 +0000

    do the same hover effect (_for element <Button>_)

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -66,7 +66,7 @@ export function Navbar() {
                     className="flex-grow"
                     autoFocus
                 />
-                <Button size="icon" onClick={handleSearch}>
+                <Button size="icon" onClick={handleSearch} className="rounded-full">
                     <Search className="h-5 w-5" />
                 </Button>
             </div>
