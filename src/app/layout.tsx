18930beee94d27ae8dbf531e8815b14eca1f6cdc
Abhishek Poshwal <abhishek.poshwal987@gmail.com>
2025-09-09 07:46:09 +0000

    add little padding on top (_for element <SearchBar>_)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ function LayoutContent({ children }: { children: React.ReactNode }) {
       <div className="flex min-h-screen flex-col">
         <Navbar />
         {showSearchBar && (
-          <div className="sticky top-16 z-40 py-2">
+          <div className="sticky top-16 z-40 py-4">
             <SearchBar />
           </div>
         )}
