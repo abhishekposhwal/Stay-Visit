8d37d82585c558dd32393c6b537b2937cd829634
Abhishek Poshwal <abhishek.poshwal987@gmail.com>
2025-09-09 07:06:33 +0000

    add border (_for element div_)

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -179,7 +179,7 @@ export default function ProfilePage() {
               {activeTab === 'about' && (
                 <div>
                   <h1 className="text-xl md:text-2xl font-bold mb-6">About me</h1>
-                  <div className="lg:p-8 lg:rounded-xl lg:border">
+                  <div className="p-6 md:p-8 rounded-xl border">
                       <h2 className="text-lg md:text-xl font-bold mb-2">Complete your profile</h2>
                       <p className="text-sm text-muted-foreground mb-6">Your Airbnb profile is an important part of every reservation. Create yours to help other hosts and guests get to know you.</p>
                       
