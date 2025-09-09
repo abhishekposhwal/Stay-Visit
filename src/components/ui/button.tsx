9af0fd54e55d90f1bea8e42716624132d87bd5f1
Abhishek Poshwal <abhishek.poshwal987@gmail.com>
2025-09-09 07:44:38 +0000

    make this circle proper round it look like ovel shape make nesseaerry ch

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -23,7 +23,7 @@ const buttonVariants = cva(
       size: {
         default: "h-auto px-4 py-2 rounded-xl",
         sm: "h-auto px-3 py-1.5 text-sm rounded-xl",
-        icon: "h-10 w-10",
+        icon: "h-10 w-10 p-0",
       },
     },
     defaultVariants: {
