ed2c845bd0ff557a6012861ac1a34a589926456b
Abhishek Poshwal <abhishek.poshwal987@gmail.com>
2025-09-12 15:18:10 +0000

    remove extra space from left and right side (_for element div_)

--- src/components/layout/SearchBar.tsx
+++ src/components/layout/SearchBar.tsx
@@ -179,7 +179,7 @@ export function SearchBar() {
             <Popover open={isDatePopoverOpen} onOpenChange={setIsDatePopoverOpen}>
                 <PopoverTrigger asChild>
                     <div className="flex-1 flex cursor-pointer">
-                        <div className="flex-1 text-left pl-4">
+                        <div className="flex-1 text-left pl-6">
                             <label className="block text-xs font-bold text-foreground/80">Check in</label>
                             <div className="text-sm text-muted-foreground">
                                 {date?.from ? format(date.from, 'LLL dd') : <span>Add dates</span>}
@@ -224,7 +224,7 @@ export function SearchBar() {
             
             <Separator orientation="vertical" className="h-8" />
 
-            <div className="flex-1 flex items-center justify-between pl-4">
+            <div className="flex-1 flex items-center justify-between pl-4 pr-2">
                 <Popover open={isGuestPopoverOpen} onOpenChange={setIsGuestPopoverOpen}>
                     <PopoverTrigger asChild>
                         <div className="text-left w-full cursor-pointer">
