175bba580b37dfa881460e3b9b2c1e9d75aeec2f
Abhishek Poshwal <abhishek.poshwal987@gmail.com>
2025-09-12 15:07:20 +0000

    improve the spacing of the search bar (_for element div_)

--- src/components/layout/SearchBar.tsx
+++ src/components/layout/SearchBar.tsx
@@ -159,7 +159,7 @@ export function SearchBar() {
             </div>
         ) : (
             <>
-            <div className="flex-[1.25] relative">
+            <div className="flex-[1.5] relative">
                 <label htmlFor="destination" className="block text-xs font-bold text-left pl-6 text-foreground/80">
                 Where
                 </label>
@@ -179,7 +179,7 @@ export function SearchBar() {
             <Popover open={isDatePopoverOpen} onOpenChange={setIsDatePopoverOpen}>
                 <PopoverTrigger asChild>
                     <div className="flex-1 flex cursor-pointer">
-                        <div className="flex-1 text-left pl-4">
+                        <div className="flex-1 text-left px-4">
                             <label className="block text-xs font-bold text-foreground/80">Check in</label>
                             <div className="text-sm text-muted-foreground">
                                 {date?.from ? format(date.from, 'LLL dd') : <span>Add dates</span>}
@@ -224,7 +224,7 @@ export function SearchBar() {
             
             <Separator orientation="vertical" className="h-8" />
 
-            <div className="flex-1 flex items-center justify-between pr-2">
+            <div className="flex-1 flex items-center justify-between">
                 <Popover open={isGuestPopoverOpen} onOpenChange={setIsGuestPopoverOpen}>
                     <PopoverTrigger asChild>
                         <div className="text-left w-full pl-4 cursor-pointer">
