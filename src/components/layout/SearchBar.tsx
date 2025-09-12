28f14675d25f9abe227f9c417cb358ded61a3ddb
Abhishek Poshwal <abhishek.poshwal987@gmail.com>
2025-09-12 15:25:51 +0000

    make this same size of checkout (_for element <SlotClone>_)

--- src/components/layout/SearchBar.tsx
+++ src/components/layout/SearchBar.tsx
@@ -201,8 +201,8 @@ export function SearchBar() {
             <Separator orientation="vertical" className="h-8" />
             <Popover>
                 <PopoverTrigger asChild>
-                     <div className="flex-1 flex cursor-pointer">
-                        <div className="flex-1 text-left px-4">
+                     <div className="flex-1 flex cursor-pointer pl-2">
+                        <div className="flex-1 text-left">
                             <label className="block text-xs font-bold text-foreground/80">Check out</label>
                             <div className="text-sm text-muted-foreground">
                                 {date?.to ? format(date.to, 'LLL dd') : <span>Add dates</span>}
@@ -307,3 +307,5 @@ export function SearchBar() {
     </div>
   );
 }
+
+    
